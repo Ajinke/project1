fc3ef98fa29b41211875d280d7adfda4b614bbda
Ajinke pensalwar <apensalwar97@gmail.com>
2020-07-19 08:07:24 +0530

    make it better

---
+++ src/components/layout/Index.js
@@ -0,0 +1,18 @@
+import React from "react";
+import Tracks from "../tracks/Tracks";
+import Search from "../tracks/Search";
+
+
+const Index=()=> {
+    return (
+        <>
+            <Search />
+            <Tracks />
+            
+        </>
+            
+        
+    );
+}
+
+export default Index;
---
+++ src/components/layout/Navbar.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+const Navbar=()=> {
+    return (
+        <nav className="navbar navbar-dark bg-warning  mb-5">
+            <span className="navbar-brand mb-0 h1 text-white mx-auto">LyricsFinder</span>
+
+        </nav>
+        
+            
+        
+    )
+}
+
+export default Navbar;
